cad6bf22814c243d5586046aaefb5132d82760de
Jun-Sours <cach456@gmail.com>
2020-12-29 23:30:08 +0900

    user input text -> hex

--- main.py
+++ main.py
@@ -21,3 +21,10 @@ if response.json().get('result_code') == 0:
     print('메시지를 성공적으로 보냈습니다.')
 else:
     print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.json()))
+
+
+text_message = input()
+
+hex_text = text_message.encode("hex")
+
+print(hex_text)
