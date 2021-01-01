c40973f715b051b9536e39fc891480b12cff0dc0
Jun_Sours <cach456@gmail.com>
2021-01-01 23:01:41 +0900

    __main__, user_defined message input

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
-#kakao talk hexa_editer
+#kakao talk hexa_message_editer
 
 import json
 import requests
 
 url = "https://kapi.kakao.com/v2/api/talk/memo/send?template_id=43550"
-user_token ="HYjzYyczoZIvZ47xeAimjc6p7pwMGK8PzjRbWQopcBMAAAF2p9958g"
+user_token ="4MqXqMHry4e6onw3atsAJQzM2CF6eZnKPNXpnwo9c5sAAAF2vipPGQ"
 # 사용자 토큰
 headers = {
     "Authorization": "Bearer " + user_token
 }
 
-payload = {
-    'template_id' : {43550},
-    'template_args' : '{"name": "테스트 제목"}'
-}
-# 카카오톡 메시지 전송
-response = requests.post(url, headers=headers, data=payload)
-print(response.status_code)
-if response.json().get('result_code') == 0:
-    print('메시지를 성공적으로 보냈습니다.')
-else:
-    print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.json()))
+def mesaage_box(hex_message):
+    payload = {
+        'template_id' : {43550},
+        'template_args' : {'name': "hex_message"}
+    }
+    return payload
 
+def send_message(hex_message,headers):# 카카오톡 메시지 전송
+    payload = mesaage_box(hex_message)
+    #payload['template_args'][name]
+    response = requests.post(url, headers=headers, data=payload)
+    print(response.status_code)
+    if response.json().get('result_code') == 0:
+        print('메시지를 성공적으로 보냈습니다.')
+    else:
+        print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.json()))
 
-text_message = input()
+def input_encoding():
+    text_message = input()
+    hex_message = text_message.encode('UTF-8-SIG').hex()
+    return hex_message
 
-hex_text = text_message.encode('UTF-8-SIG').hex()
+if __name__ == "__main__":
+    hex_message = input_encoding()
+    send_message(hex_message,headers)
 
-print(hex_text)
